dd4fa3f078a84987bac71fc3228725ecbff8fdbf
Faaris Muwaffaq <85434419+risproject@users.noreply.github.com>
2023-03-01 23:16:36 +0700

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,21 @@
+
+//navbar
+const menu = document.querySelector('#menu')
+const navMenu = document.querySelector('#nav-menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('menu-active')
+    navMenu.classList.toggle('hidden')
+});
+
+// navbar scroll
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+  
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add('navbar-fixed');
+    } else {
+      header.classList.remove('navbar-fixed');
+    }
+  };
