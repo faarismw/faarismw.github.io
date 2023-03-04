6139421837d603f267aedf26349af23ef07a6154
Faaris Muwaffaq <85434419+risproject@users.noreply.github.com>
2023-03-04 17:10:54 +0700

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -8,14 +8,65 @@ menu.addEventListener('click', function() {
     navMenu.classList.toggle('hidden')
 });
 
+//klik menu
+window.addEventListener('click' , function(e) {
+  if(e.target != menu && e.target != navMenu){
+    menu.classList.remove('menu-active')
+    navMenu.classList.add('hidden')
+  }
+})
+
 // navbar scroll
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
   
     if (window.pageYOffset > fixedNav) {
       header.classList.add('navbar-fixed');
+      toTop.classList.remove('hidden');
+
     } else {
       header.classList.remove('navbar-fixed');
+      toTop.classList.add('hidden');
     }
   };
+  
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#darkMode');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
+
+const alertClose = document.querySelector('.alert-close');
+const alert = document.querySelector('.alert');
+
+alertClose.addEventListener('click' , function() {
+  alert.classList.toggle('hidden')
+});
